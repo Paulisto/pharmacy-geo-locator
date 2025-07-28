80c10cb7a91196dbae44beb1aacb8322c815e0a2
Paul Kalibbala <35539407+Paulisto@users.noreply.github.com>
2025-07-28 20:54:37 +0300

    Ability to see which pharmacies are open and their wrk hours

--- source/js/map.js
+++ source/js/map.js
@@ -168,7 +168,7 @@ function addMarker(pharmacy) {
         if (openStatus === true) {
             isOpenSpan = `<span class="text-success"><b><i class="fa fa-clock-o" aria-hidden="true"></i> Open now</b></span>`;
         } else if (openStatus === false) {
-            isOpenSpan = `<span class="text-danger"><b><i class="fa fa-clock-0" aria-hidden="true"></i> Closed</b></span>`;
+            isOpenSpan = `<span class="text-danger"><b><i class="fa fa-clock-o" aria-hidden="true"></i> Closed</b></span>`;
         }
         // if openStatus is null, it will show nothing
     }
